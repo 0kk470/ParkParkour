023a2c35cea750a4e4b6bdf3814196798f412ce9
0kk470 <ookk47oo@gmail.com>
2018-04-20 09:59:17 +0800

    Park BG

---
+++ Assets/Script/Game/TileBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour {
+
+	// Use this for initialization
+	protected virtual void  Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update ()
+    {
+		
+	}
+
+    public virtual void OnBecameInvisible()
+    {
+        
+    }
+
+}
