d8492dfea347da033efde6e61a1c9f70f141243b
0kk470 <ookk47oo@gmail.com>
2018-05-03 22:02:08 +0800

    merge

--- Assets/Script/Game/OnGroundDetect.cs
+++ Assets/Script/Game/OnGroundDetect.cs
@@ -15,7 +15,7 @@ public class OnGroundDetect : MonoBehaviour {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
             Debug.Log("地面");
-            if(player.GetPlayerState() != PlayerState.Idle)
+            //if(player.GetPlayerState() != PlayerState.Idle)
               player.SetState(PlayerState.Run);
         }
     }
